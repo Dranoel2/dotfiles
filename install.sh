249b93e71a7cf99768b1f26b74b8c04be3507155
Dranoel2 <45979197+Dranoel2@users.noreply.github.com>
2022-01-07 19:30:39 +0000

    add alacritty config

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 cd $(dirname "$0")
 
 ln -sf $(pwd)/zshrc.zsh ../.zshrc
+
 mkdir -p ../.xmonad
 ln -sf $(pwd)/xmonad.hs ../.xmonad/xmonad.hs
+
 ln -sf $(pwd)/xmobarrc  ../.xmobarrc
+
 mkdir -p ../.config/rofi
 ln -sf $(pwd)/rofiConfig.rasi ../.config/rofi/config.rasi
 ln -sf $(pwd)/rofiTheme.rasi ../.config/rofi/dranoelsTheme.rasi
+
+ln -sf $(pwd)/alacritty.yml ../.alacritty.yml
